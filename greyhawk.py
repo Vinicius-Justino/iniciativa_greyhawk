ed26ede874621e47dec6f0392f19e8c65c653326
Vinicius-Justino <68748684+Vinicius-Justino@users.noreply.github.com>
2023-12-03 13:28:29 -0300

    Add files via upload

--- greyhawk.py
+++ greyhawk.py
@@ -1,108 +1,174 @@
 from random import randint
 
+atuantes = []
+destrezas_atuantes = {}
+
 def main():
-    jogadores = getPlayers()
-    inimigos = getEnemies()
+    anotaJogadores()
+    anotaInimigos()
 
     while True:
-        acoes = getActions(jogadores + inimigos)
-        if len(acoes) == 0:
+        mostraAtuantes()
+
+        print(
+        """
+        [a]dicionar atuante
+        [r]emover atuante
+        [g]erar iniciativa
+        [f]inalizar o combate
+        """)
+        comando = input("Comando: ").lower()
+
+        if comando == "a":
+            tipo = input("[j]ogador ou [i]nimigo? R: ").lower()
+
+            if tipo == "j":
+                anotaJogadores()
+            elif tipo == "i":
+                anotaInimigos()
+        elif comando == "r":
+            apagaAtuantes()
+        elif comando == "g":
+            iniciativaGW()
+        elif comando == "f":
             break
 
-        for iniciativa, atuantes in gwInitiative(acoes).items():
-            print(f"{iniciativa}: {atuantes}")
 
+def anotaJogadores():
+    """
+    Atualiza a lista de jogadores
+    """
+    global atuantes, destrezas_atuantes
 
-def getPlayers():
-    """
-    Pega a lista de jogadores
-    """
-    l = []
-    i = 1
+    i = 0
     while True:
-        name = input(f"jogador {i}: ")
-        if name == "-":
-            break
-
-        l.append(name)
         i += 1
-
-    return l
-
-def getEnemies():
-    """
-    Pega a orda de inimigos
-    """
-    enemies = []
-    while True:
-        name = input(f"criatura: ")
-        if name == "-":
+        
+        nome = input(f"Jogador {i}: ")
+        if nome == "-":
             break
 
-        num = int(input("quantidade: "))
-        for i in range(num):
-            enemies.append(f"{name} {i + 1}")
+        atuantes.append(nome)
+        destrezas_atuantes[nome] = int(input("Valor bruto de Destreza: "))
 
-    return enemies
-
-
-def getActions(actors):
-    """
-    Pega as ações de cada atuante
-    """
-
-    # Mostra a lista de atuantes
-    for i in range(len(actors)):
-        print(f"{i + 1}: {actors[i]}", end=" | ")
-    
-    # Mostra a lista de ações
-    print("""
-    [r]anged attack
-    [m]ove/swap/other
-    melee [a]ttack
-    cast a [s]pell
-    """)
-
-    # Pega as ações
-    acts = {}
-    while True:
-        actor = int(input("inimigo/jogador: ")) - 1
-        if actor < 0:
-            break
-
-        acts[actors[actor]] = input("ações: ")
         print("\n")
 
-    return acts
+    print("\n")
 
-def gwInitiative(acts):
+def anotaInimigos():
     """
-    Calcula a iniciativa usando o método de Greyhawk.
+    Atualiza a horda de inimigos
     """
+    global atuantes
 
-    # Inicia cada atuante com 0 de iniativa
-    initiative = {}
-    for actor in acts.keys():
-        initiative[actor] = 0
+    while True:
+        nome = input("Criatura: ")
+        if nome == "-":
+            break
 
-    # Calcula o valor de cada ação de cada atuante
-    for actor in acts.keys():
-        dice = {"r": 4, "m": 6, "a": 8, "s": 10}
+        dex = int(input("Valor bruto de Destreza: "))
 
-        for act in acts[actor]:
-            initiative[actor] += randint(1, dice[act] + 1)
+        quantidade = int(input("Quantidade: "))
+        for i in range(quantidade):
+            atuantes.append(f"{nome} {i + 1}")
+            destrezas_atuantes[f"{nome} {i + 1}"] = dex
 
-    # Organiza o turno em ordem de iniciativa
-    turn = {}
-    for i in range(1, 31):
-        for actor, value in initiative.items():
-            if value == i:
-                try:
-                    turn[str(i)] += f", {actor}"
-                except:
-                    turn[str(i)] = actor
+        print("\n")
+    
+    print("\n")
 
-    return turn
+def mostraAtuantes():
+    """
+    Mostra a lista atual de atuantes na cena
+    """
+    global atuantes
+
+    for i in range(len(atuantes)):
+        print(f"{i + 1}: {atuantes[i]}", end=" | ")
+    
+    print("\n\n")
+
+def apagaAtuantes():
+    """
+    Apaga itens de atuantes pelo índice
+    """
+    global atuantes, destrezas_atuantes
+
+    removidos = [atuantes[int(n) - 1] for n in input("Números dos removidos: ").split()]
+
+    for nome in removidos:
+        destrezas_atuantes.pop(atuantes[atuantes.index(nome)])
+        atuantes.remove(nome)
+
+
+def iniciativaGW():
+    """
+    Recebe as ações de cada atuante e calcula a iniciativa usando o método de Greyhawk
+    """
+    global atuantes, destrezas_atuantes
+
+    iniciativas_atuantes = {}
+    dados = {"r": 4, "m": 6, "a": 8, "s": 10}
+
+    # Recebe a lista de ações e calcula a iniciativa
+    print("""
+    Ações: 
+        ataque [r]anged
+        [m]ovimento / trocar equipamento / outro
+        [a]taque melee
+        [s]pell
+    
+    Situação especial:
+        surpreendido[!]
+    """)
+
+    while True:
+        indices_atuantes = input("Números dos atuantes: ")
+        if indices_atuantes == "0":
+            break
+
+        conjunto_atuantes = [atuantes[int(indice_atuante) - 1] for indice_atuante in indices_atuantes.split()]
+        acoes = input("Ações: ").lower()
+
+        for atuante in conjunto_atuantes:
+            iniciativa = 0
+            for acao in acoes:
+                if acao == "!":
+                    iniciativa += 10
+                elif acao in dados.keys():
+                    iniciativa += randint(1, dados[acao])
+            
+            iniciativas_atuantes[atuante] = iniciativa
+    
+    # Organiza todas as iniciativas
+    ordem_rodada = []
+    for atuante, iniciativa in iniciativas_atuantes.items():
+        i = 0
+        atuante_inserido = False
+        while i < len(ordem_rodada) and not atuante_inserido:
+            if iniciativas_atuantes[ordem_rodada[i]] > iniciativa:
+                ordem_rodada.insert(i, atuante)
+
+                atuante_inserido = True
+            elif iniciativas_atuantes[ordem_rodada[i]] == iniciativa:
+                if destrezas_atuantes[ordem_rodada[i]] == destrezas_atuantes[atuante]:
+                    if randint(0, 1):
+                        ordem_rodada.insert(i, atuante)
+
+                        atuante_inserido = True
+                elif destrezas_atuantes[ordem_rodada[i]] < destrezas_atuantes[atuante]:
+                    ordem_rodada.insert(i, atuante)
+
+                    atuante_inserido = True
+
+            i += 1
+        
+        if not atuante_inserido:
+            ordem_rodada.append(atuante)
+
+    # Mostra a ordem da rodada
+    for nome in ordem_rodada:
+        print(nome)
     
 
 main()
